317644166fc7bd3dac3933afbb92f8c660b130e3
Do Ngo <bahuynh619@gmail.com>
2025-06-23 06:08:12 +0700

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -18,4 +18,24 @@ int main(){
 
 
 
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){   
+        a[i]=a[i]*a[i];                 //print square elements in array
+    }
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+}
+
+
+
 * 
